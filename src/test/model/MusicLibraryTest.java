baaaca797a9ce96eddadeb47ca62c6e2f808eae8
triston <tristont08tw@gmail.com>
2022-10-17 01:45:12 -0700

    Added more detailed tests.

--- src/test/model/MusicLibraryTest.java
+++ src/test/model/MusicLibraryTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -26,7 +28,7 @@ class MusicLibraryTest {
     }
 
     @Test
-    public void TestAddToList() {
+    public void TestAddSong() {
         Song song2 = new Song("O'Canada", "1990", "Justin Bieber", "Cool Song");
         Song song3 = new Song("What does the Fox Say?", "203", "Pewdiepie", "Very Nice!");
         this.musicList.addSong(song2);
@@ -35,9 +37,8 @@ class MusicLibraryTest {
     }
 
     @Test
-    public void TestRemoveFromList() {
+    public void TestRemoveSong() {
         Song song2 = new Song("O'Canada", "1990", "Justin Bieber", "Cool Song");
-
         Song song3 = new Song("What does the Fox Say?", "203", "Pewdiepie", "Very Nice!");
         this.musicList.addSong(song2);
         this.musicList.addSong(song3);
@@ -48,10 +49,70 @@ class MusicLibraryTest {
     }
 
     @Test
-    public void TestSongConstructor() {
+    public void TestGetSongName() {
         assertEquals("Happy Birthday", song1.getSongName());
+    }
+
+    @Test
+    public void GetSongDate() {
         assertEquals("2000", song1.getSongDate());
+    }
+
+    @Test
+    public void GetSongArtist() {
         assertEquals("Jay-Z", song1.getSongArtist());
+    }
+
+    @Test
+    public void GetSongInfo() {
         assertEquals("Listen on birthday!", song1.getSongInfo());
     }
+
+    @Test
+    public void TestGetPlayListName() {
+        assertEquals("Christine's list", musicList.getName());
+    }
+
+    @Test
+    public void TestGetSongs() {
+        assertEquals(1, musicList.getSongs().size());
+    }
+
+    @Test
+    public void TestSetName() {
+        musicList.setName("New Name");
+        assertEquals("New Name", musicList.getName());
+    }
+
+    @Test
+    public void TestGetSongsInfo() {
+        Song song2 = new Song("O'Canada", "1990", "Justin Bieber", "Cool Song");
+        Song song3 = new Song("What does the Fox Say?", "203", "Pewdiepie", "Very Nice!");
+        this.musicList.addSong(song2);
+        this.musicList.addSong(song3);
+        ArrayList<String> testing = new ArrayList<>();
+        testing.add("Name: Happy Birthday");
+        testing.add("Date: 2000");
+        testing.add("Artist: Jay-Z");
+        testing.add("Info: Listen on birthday!");
+        assertTrue(testing.equals(musicList.getSongsInfo("Happy Birthday")));
+    }
+
+
+    @Test
+    public void TestGetSongsNames() {
+        Song song2 = new Song("O'Canada", "1990", "Justin Bieber", "Cool Song");
+        Song song3 = new Song("What does the Fox Say?", "203", "Pewdiepie", "Very Nice!");
+        this.musicList.addSong(song2);
+        this.musicList.addSong(song3);
+        ArrayList<String> testArray = new ArrayList<>();
+        testArray.add("Happy Birthday");
+        testArray.add("O'Canada");
+        testArray.add("What does the Fox Say?");
+        assertTrue(testArray.equals(musicList.getSongsNames()));
+    }
+
+
+
+
 }
