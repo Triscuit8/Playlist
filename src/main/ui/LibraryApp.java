ee2c80e68195749e77ef2515e106cee748af6946
Triscuit8 <tristontsui@gmail.com>
2022-11-17 20:12:06 -0800

    added gui.

--- src/main/ui/LibraryApp.java
+++ src/main/ui/LibraryApp.java
@@ -7,11 +7,42 @@ import persistence.JsonWriter;
 //import persistence.JsonReader2;
 //import persistence.JsonWriter2;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class LibraryApp {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+
+public class LibraryApp extends JFrame implements ActionListener {
+
+
+    JFrame frame;
+    JTextField fieldN;
+    JTextField fieldD;
+    JTextField fieldA;
+    JTextField fieldI;
+
+    JButton buttonAdd;
+    JButton buttonDelete;
+    JButton buttonSave;
+    JButton buttonLoad;
+
+    JLabel label1;
+    JLabel label2;
+
+    JPanel panel1;
+
+    Icon image1;
+
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 700;
+
 
     Scanner userInput = new Scanner(System.in);
     Scanner userScreenNewName = new Scanner(System.in);
@@ -32,69 +63,78 @@ public class LibraryApp {
     private JsonWriter jsonWriter2;
     private JsonReader jsonReader2;
 
+    Scanner uiInput = new Scanner(System.in);
+    String userChoice;
 
     //EFFECTS: Constructs a library
     @SuppressWarnings({"checkstyle:MethodLength", "checkstyle:SuppressWarnings"})
     public LibraryApp() {
+
+
+        initializeGraphics();
+        theMainMenu();
 
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
         jsonWriter2 = new JsonWriter(JSON_STORE2);
         jsonReader2 = new JsonReader(JSON_STORE2);
 
-        while (stayOnMenu) {
 
-            theMainMenu();
-
-            String userChoice = userInput.nextLine().toLowerCase();
-
-            if (userChoice.equals("a")) {
-
-                renameMusicListMenu();
-
-            } else if (userChoice.equals("b")) {
-
-                addSongMenu();
-
-            } else if (userChoice.equals("c")) {
-
-                removeSongMenu();
-
-            } else if (userChoice.equals("d")) {
-
-                getSongsInfoMenu();
-
-            } else if (userChoice.equals("e")) {
-
-                shuffleSongsMenu();
-
-            } else if (userChoice.equals("f")) {
-
-                addSongToFavListMenu();
-
-            } else if (userChoice.equals("g")) {
-
-                removeSongFromFavListMenu();
-
-            } else if (userChoice.equals("q")) {
-
-                quitApp();
-
-            } else if (userChoice.equals("s")) {
-
-                saveMusicList();
-                saveFavMusicList();
-
-            } else if (userChoice.equals("l")) {
-
-                loadMusicList();
-                loadFavMusicList();
-
-            } else {
-                invalidKeyMain();
-            }
-        }
-    }
+//        while (stayOnMenu) {
+//
+//            theMainMenu();
+//
+//            userChoice = userInput.nextLine().toLowerCase();
+//
+//            //String userChoice = uiInput.nextLine().toLowerCase();
+//
+//            if (userChoice.equals("a")) {
+//
+//                renameMusicListMenu();
+//
+//            } else if (userChoice.equals("b")) {
+//
+//                addSongMenu();
+//
+//            } else if (userChoice.equals("c")) {
+//
+//                removeSongMenu();
+//
+//            } else if (userChoice.equals("d")) {
+//
+//                getSongsInfoMenu();
+//
+//            } else if (userChoice.equals("e")) {
+//
+//                shuffleSongsMenu();
+//
+//            } else if (userChoice.equals("f")) {
+//
+//                addSongToFavListMenu();
+//
+//            } else if (userChoice.equals("g")) {
+//
+//                removeSongFromFavListMenu();
+//
+//            } else if (userChoice.equals("q")) {
+//
+//                quitApp();
+//
+//            } else if (userChoice.equals("s")) {
+//
+//                saveMusicList();
+//                saveFavMusicList();
+//
+//            } else if (userChoice.equals("l")) {
+//
+//                loadMusicList();
+//                loadFavMusicList();
+//
+//            } else {
+//                invalidKeyMain();
+//            }
+//        }
+}
 
 
     //EFFECTS: Prints out a screen for the main menu of the program
@@ -150,6 +190,11 @@ public class LibraryApp {
         musicList.addSong(newSong);
         System.out.println("Song has been added!");
         System.out.println('\n');
+    }
+
+    public void addSongMenu(Song song) {
+        musicList.addSong(song);
+
     }
 
     //EFFECTS: Prints out a menu for hitting an invalid key
@@ -258,4 +303,159 @@ public class LibraryApp {
             System.out.println("Unable to read from file: " + JSON_STORE2);
         }
     }
+
+
+
+    private void initializeGraphics() {
+
+        Border border = BorderFactory.createLineBorder(Color.GREEN);
+
+        image1 = new ImageIcon(getClass().getResource("mu.jpeg"));
+
+        label1 = new JLabel();
+        label1.setText("Welcome! Your songs are " + songNamesUI());
+        label1.setVisible(true);
+        label1.setBorder(border);
+        label1.setOpaque(true);
+        label1.setVerticalAlignment(JLabel.CENTER);
+        label1.setHorizontalAlignment(JLabel.CENTER);
+
+        label2 = new JLabel(image1);
+        label2.setBounds(0, 250, 800, 350);
+
+
+        panel1 = new JPanel();
+        panel1.setBackground(Color.LIGHT_GRAY);
+        panel1.setBounds(0, 0, 800, 250);
+        panel1.setLayout(new BorderLayout());
+
+
+        buttonAdd = new JButton();
+        buttonAdd.setBounds(50, 300, 80, 60);
+        buttonAdd.addActionListener(this);
+        buttonAdd.setText("Add");
+        buttonAdd.setFocusable(false);
+        buttonAdd.setBackground(Color.LIGHT_GRAY);
+        buttonAdd.setOpaque(true);
+        buttonAdd.setFont(new Font ("Comic Sans", Font.BOLD, 12));
+
+        buttonDelete = new JButton();
+        buttonDelete.setBounds(200, 300, 80, 60);
+        buttonDelete.addActionListener(this);
+        buttonDelete.setText("Remove");
+        buttonDelete.setFocusable(false);
+        buttonDelete.setBackground(Color.LIGHT_GRAY);
+        buttonDelete.setOpaque(true);
+        buttonDelete.setFont(new Font ("Comic Sans", Font.BOLD, 12));
+
+        buttonSave = new JButton();
+        buttonSave.setBounds(50, 400, 80, 60);
+        buttonSave.addActionListener(this);
+        buttonSave.setText("Save");
+        buttonSave.setFocusable(false);
+        buttonSave.setBackground(Color.LIGHT_GRAY);
+        buttonSave.setOpaque(true);
+        buttonSave.setFont(new Font ("Comic Sans", Font.BOLD, 12));
+
+        buttonLoad = new JButton();
+        buttonLoad.setBounds(200, 400, 80, 60);
+        buttonLoad.addActionListener(this);
+        buttonLoad.setText("Load");
+        buttonLoad.setFocusable(false);
+        buttonLoad.setBackground(Color.LIGHT_GRAY);
+        buttonLoad.setOpaque(true);
+        buttonLoad.setFont(new Font ("Comic Sans", Font.BOLD, 12));
+
+        fieldN = new JTextField();
+        fieldN.setPreferredSize(new Dimension(250, 40));
+        fieldN.setBounds(400, 300, 250, 40);
+        fieldN.setText("Enter song name...");
+
+        fieldD = new JTextField();
+        fieldD.setPreferredSize(new Dimension(250, 40));
+        fieldD.setBounds(400, 350, 250, 40);
+        fieldD.setText("Enter song date...");
+
+        fieldA = new JTextField();
+        fieldA.setPreferredSize(new Dimension(250, 40));
+        fieldA.setBounds(400, 400, 250, 40);
+        fieldA.setText("Enter artist name...");
+
+        fieldI = new JTextField();
+        fieldI.setPreferredSize(new Dimension(250, 40));
+        fieldI.setBounds(400, 450, 250, 40);
+        fieldI.setText("Enter song info...");
+
+        frame = new JFrame();
+        frame.setTitle("Music Playlist");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setResizable(false);
+
+        panel1.add(label1);
+
+
+        frame.add(panel1);
+
+
+
+
+        frame.add(buttonAdd);
+        frame.add(buttonDelete);
+        frame.add(buttonSave);
+        frame.add(buttonLoad);
+
+        frame.add(fieldN);
+        frame.add(fieldD);
+        frame.add(fieldA);
+        frame.add(fieldI);
+        frame.add(label2);
+
+        frame.setLayout(null);
+
+        frame.getContentPane().setBackground(Color.WHITE);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == buttonAdd) {
+            Song newSong = new Song(fieldN.getText(), fieldD.getText(), fieldA.getText(), fieldI.getText());
+            musicList.addSong(newSong);
+            label1.setText("Welcome! Your songs are " + songNamesUI());
+        }
+        if (e.getSource() == buttonDelete) {
+            musicList.removeSong(fieldN.getText());
+            label1.setText("Welcome! Your songs are " + songNamesUI());
+        }
+        if (e.getSource() == buttonSave) {
+            saveMusicList();
+            label1.setText("Welcome! Your songs are " + songNamesUI());
+        }
+        if (e.getSource() == buttonLoad) {
+            loadMusicList();
+            label1.setText("Welcome! Your songs are " + songNamesUI());
+        }
+    }
+
+    public String addSongScreen() {
+        return "Please enter the Song's name, date, artist and info.";
+
+    }
+
+    public String removeSongScreen() {
+        return "Please enter the name of the song you wish to remove.";
+    }
+
+    public String saveListScreen() {
+        return "The list has been saved";
+    }
+
+
+    public String songNamesUI() {
+        return musicList.getSongsNames().toString();
+    }
+
+
 }
